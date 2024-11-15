a0f832a830352362e31eddc8dca4c1b72ca42571
dodoyoo <dong15903@daum.net>
2024-11-15 14:36:49 +0900

    fix swagger

--- server.ts
+++ server.ts
@@ -18,58 +18,65 @@ customLogger.customedDebug('level called customedDebug');
 customLogger.customedSilly('level called customedSilly');
 
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Express API with Swagger',
-        version: '1.0.0',
-        description: 'A simple CRUD API application made with Express and documented with Swagger',
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API with Swagger',
+    version: '1.0.0',
+    description:
+      'A simple CRUD API application made with Express and documented with Swagger',
+  },
+  servers: [
+    { url: `http://${HOST}:${port}` },
+    { url: `https://${HOST}:${port}` },
+  ],
+  basePath: '/',
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
     },
-    servers: [
-        {
-            url: `http://${HOST}:${port}`,
-        },
-    ],
-    basePath: '/',
-    components: {
-        securitySchemes: {
-            BearerAuth: {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
-            },
-        },
-    },
+  },
 };
 
 const options = {
-    swaggerDefinition,
-    apis: [
-        './src/domain/user/*.ts',
-        './src/domain/store/*.ts',
-        './src/domain/review/*.ts',
-        './src/domain/reservation/*.ts',
-    ],
+  swaggerDefinition,
+  apis: [
+    './src/domain/user/*.ts',
+    './src/domain/store/*.ts',
+    './src/domain/review/*.ts',
+    './src/domain/reservation/*.ts',
+  ],
 };
 const swaggerSpec = swaggerJSDoc(options);
 
 (async () => {
-    await AppDataSource.initialize()
-        .then(() => {
-            console.log('Data Source has been initialized!');
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
 
-            app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+      app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-            app.all('*', (req: Request, res: Response, next: NextFunction) => {
-                const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+      app.all('*', (req: Request, res: Response, next: NextFunction) => {
+        const err = new Error(`Can't find ${req.originalUrl} on this server!`);
 
-                next(err);
-            });
+        next(err);
+      });
 
-            app.listen(port, async () => {
-                customLogger.customedInfo(`Server is running on port ${port}`);
-                customLogger.customedError('This is an error message!');
-                console.log(`Swagger docs available at http://${HOST}:${port}/api-docs`);
-            });
-        })
-        .catch((error) => customLogger.customedError('Error during Data Source initialization:', error));
+      app.listen(port, async () => {
+        customLogger.customedInfo(`Server is running on port ${port}`);
+        customLogger.customedError('This is an error message!');
+        console.log(
+          `Swagger docs available at http://${HOST}:${port}/api-docs`
+        );
+      });
+    })
+    .catch((error) =>
+      customLogger.customedError(
+        'Error during Data Source initialization:',
+        error
+      )
+    );
 })();
